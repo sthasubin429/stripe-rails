df71faac17e79da7d239fb5760711f747b8fd27f
subin <subinshrestha429@gmail.com>
2022-07-08 21:53:53 +0545

    card with authentication

---
+++ app/controllers/card_controller.rb
@@ -0,0 +1,5 @@
+class CardController < ApplicationController
+  before_action :authenticate_user!
+
+  def index;end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :user
+  get "card", to: "card#index"
+
 end
