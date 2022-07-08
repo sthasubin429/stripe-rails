37d0ac48c6371327d7e673f1f5a591b64881779d
subin <subinshrestha429@gmail.com>
2022-07-08 21:52:50 +0545

    devise and rails_admin setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  devise_for :admins, path: 'admins', skip: [:registrations]
+  devise_for :users, path: 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
